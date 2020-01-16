8e95da391a51f18622a6539b06b38c854b5e3284
Michelle Cupp <shellcupp@gmail.com>
2020-01-16 15:04:12 -0600

    my card working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,17 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/shellcupp")
-  .then(response => {
-    console.log(response);
-    response.data.message.forEach(data => {
-      const newCard = Card(data);
-      entryPoint.appendChild(newCard);
-    });
-  })
-  .catch(error => {
-    console.log("The data was not returned", error);
-  });
+axios.get('https://api.github.com/users/shellcupp').then(response => {
+  // console.log(response.data);
+  const myCard = userCard(response.data)
+  addCard.appendChild(myCard)
+})
+.catch(err => {
+  console.log(err);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -34,7 +31,8 @@ axios.get("https://api.github.com/users/shellcupp")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+ const followersArray = []
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -55,33 +53,55 @@ const followersArray = [];
 </div>
 
 */
-function card(data) {
-  const
-  newCard = document.createElement('div'),
-  newImage = document.createElement('img'),
-  cardInfo = document.createElement('div'),
-  name = document.createElement('h3'),
-  username = document.createElement('p'),
-  location = document.createElement('p'),
-  profile = document.createElement('p')
-  profileLink = document.createElement('a'),
-  followers = document.createElement('p'),
-  following = document.createElement('p'),
-  bio = document.createElement('p');
+function userCard(object) {
+  // create elements
+  const newCard = document.createElement('div'),
+        newImage = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        nameH3 = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        link = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
 
-  newImage.src = data.avatar.url;
-  name.textContent = data.name;
-  username.textContent = data.login;
-  location.textContent = data.location;
-  profile.textContent = 'Profile: ';
-  profileLink.href = data.html_url;
-  profileLink.textContent = data.html_url;
-  followers.textContent = `Followers: ${data.followers}`;
-  following.textContent = `Folling: ${data.following}`;
-  bio.textContent = data.bio;
 
-  
+// add classes
+  newCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  nameH3.classList.add('name');
+  userName.classList.add('username');
+
+// text content
+  newImage.src = object.avatar_url;
+  nameH3.textContent = object.name;
+  userName.textContent = object.login;
+  location.textContent = `Location: ${object.location}`;
+  profile.textContent = `Profile: ${link}`;
+  link.textContent = object.html_url;
+  followers.textContent = `Followers: ${object.followers}`;
+  following.textContent = `Following: ${object.following}`;
+  bio.textContent = `Bio: ${object.bio}`;
+
+// append to parents
+  newCard.appendChild(newImage);
+  newCard.appendChild(cardInfo);
+  cardInfo.appendChild(nameH3);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(link);
+
+  return newCard;
 }
+
+const addCard = document.querySelector('.cards')
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
