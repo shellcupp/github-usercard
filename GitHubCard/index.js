f8167b00b1b34ea968b002a18788b366c5b219c9
Michelle Cupp <shellcupp@gmail.com>
2020-01-16 17:19:11 -0600

    meets mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,13 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/shellcupp').then(response => {
-  // console.log(response.data);
+axios.get('https://api.github.com/users/shellcupp')
+.then(response => {
+   console.log(response.data);
   const myCard = userCard(response.data)
   addCard.appendChild(myCard)
 })
-.catch(err => {
-  console.log(err);
+.catch(error=> {
+  console.log(error);
 })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -31,7 +32,22 @@ axios.get('https://api.github.com/users/shellcupp').then(response => {
           user, and adding that card to the DOM.
 */
 
- const followersArray = []
+ const followersArray = ['tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
+
+followersArray.forEach(person => {
+axios.get('https://api.github.com/users/' + person).then(response => {
+  const followerCard = userCard(response.data)
+  addCard.appendChild(followerCard);
+})
+.catch(err => {
+  console.log(err);
+})
+})
 
 
 /* Step 3: Create a function that accepts a single object as its only argument,
@@ -80,7 +96,8 @@ function userCard(object) {
   userName.textContent = object.login;
   location.textContent = `Location: ${object.location}`;
   profile.textContent = `Profile: ${link}`;
-  link.textContent = object.html_url;
+  link.textContent = object.url;
+  link.href = object.html_url;
   followers.textContent = `Followers: ${object.followers}`;
   following.textContent = `Following: ${object.following}`;
   bio.textContent = `Bio: ${object.bio}`;
